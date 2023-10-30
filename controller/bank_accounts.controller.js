2f19b794204f1640fe00497fb54c5339287f9c77
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-10-30 14:40:10 +0700

    add create bank account

--- controller/bank_accounts.controller.js
+++ controller/bank_accounts.controller.js
@@ -2,27 +2,45 @@ const { ResponseTemplate } = require("../helper/template.helper");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-async function create(req,res) {
-  const {user_id,bank_name, bank_account_number} = req.body
-  const payload = {
-    user_id: parseInt(user_id),
-    bank_name,
-    bank_account_number
-  }
+async function create(req, res) {
+  const {user_id,  bank_name, bank_account_number, balance } = req.body;
+  // const {user_id} = req.user.id
+
+  // Pastikan Anda memiliki akses ke 'prisma' dan telah menginisialisasinya sebelumnya
+  const prisma = new PrismaClient();
+
   try {
+
     const bank_account = await prisma.bankAccount.create({
-      data: payload,
+      data: {
+        user_id: parseInt(user_id),
+        bank_name,
+        bank_account_number: parseInt(bank_account_number),
+        balance: parseInt(balance),
+        // user: {
+        //   connect: {
+        //     id: user_id, // Menggunakan user_id, // Menggunakan user_id dari req.body
+        //     // name: name,
+        //   },
+        // },
+      },
+      // include: {
+      //   user: true,
+      // },
     });
+
     let resp = ResponseTemplate(bank_account, "success", null, 200);
     res.json(resp);
-    return;
   } catch (error) {
+    console.error(error); // Cetak pesan kesalahan untuk debug
     let resp = ResponseTemplate(null, "internal server error", error, 500);
     res.json(resp);
-    return;
+  } finally {
+    await prisma.$disconnect(); // Tutup koneksi Prisma
   }
 }
 
+
 module.exports = {
-  create
-}
+  create,
+};
