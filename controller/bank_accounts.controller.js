d9f4b26e0127a3fe9b4012144e2565bb8e02036a
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-02 06:45:51 +0700

    tambah select pada function create

--- controller/bank_accounts.controller.js
+++ controller/bank_accounts.controller.js
@@ -3,14 +3,13 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function create(req, res) {
-  const {user_id,  bank_name, bank_account_number, balance } = req.body;
+  const { user_id, bank_name, bank_account_number, balance } = req.body;
   // const {user_id} = req.user.id
 
   // Pastikan Anda memiliki akses ke 'prisma' dan telah menginisialisasinya sebelumnya
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
 
   try {
-
     const bank_account = await prisma.bankAccount.create({
       data: {
         user_id: parseInt(user_id),
@@ -24,22 +23,33 @@ async function create(req, res) {
         //   },
         // },
       },
-      // include: {
-      //   user: true,
-      // },
+      select: {
+        id: true,
+        user_id: true,
+        bank_name: true,
+        bank_account_number: true,
+        balance: true,
+      },
     });
 
-    let resp = ResponseTemplate(bank_account, "success", null, 200);
-    res.json(resp);
+    if (bank_account ) {
+      let resp = ResponseTemplate(bank_account, "success", null, 200);
+      res.json(resp);
+      return;
+    } else {
+      let resp = ResponseTemplate(null, "internal server error", null, 500);
+      res.json(resp);
+      return;
+    }
+
+    // let resp = ResponseTemplate(bank_account, "success", null, 200);
+    // res.json(resp);
   } catch (error) {
     console.error(error); // Cetak pesan kesalahan untuk debug
     let resp = ResponseTemplate(null, "internal server error", error, 500);
     res.json(resp);
-  } finally {
-    await prisma.$disconnect(); // Tutup koneksi Prisma
   }
 }
-
 
 module.exports = {
   create,
