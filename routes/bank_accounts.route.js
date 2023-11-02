c00664ae0ce0f74c9bddfeb0c74597e43be01ee1
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-02 12:21:25 +0700

    tambah get by id

--- routes/bank_accounts.route.js
+++ routes/bank_accounts.route.js
@@ -1,6 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const {create} = require("../controller/bank_accounts.controller");
+const {create, getAll, getById} = require("../controller/bank_accounts.controller");
 
 router.post("/", create);
+// router.get("/", getAll);
+
+/**
+ * @swagger
+ * /api/v1/accounts:
+ *   get:
+ *     summary: example to get accounts
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal Server Error
+ */
+router.get('/', getAll)
+
+/**
+ * @swagger
+ * /api/v1/accounts/{id}:
+ *   get:
+ *     summary: Get one user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal Server Error
+ */
+
+router.get('/:id', getById)
 module.exports = router;
