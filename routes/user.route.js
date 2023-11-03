bc2f080693b07849ad0720d21345328819a3d093
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-03 20:49:22 +0700

    merpaihkan baris kode

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const { test, create, getAll, getById, updateById, deleteById } = require('../controller/user.constroller');
+const {
+  test,
+  create,
+  getAll,
+  getById,
+  updateById,
+  deleteById,
+} = require("../controller/user.constroller");
 const { PrintSuccess, CheckPostReq } = require("../middleware/middleware");
 
 // router.use(PrintSuccess)
 // router.get("/", test);
-router.post("/", CheckPostReq, create);
+// router.post("/", CheckPostReq, create);
 // router.get('/', getAll);
 // router.get('/:id', getById);
-router.put('/:id', updateById);
-router.delete('/:id', deleteById);
+router.put("/:id", updateById);
+router.delete("/:id", deleteById);
 
 /**
  * @swagger
  * /api/v1/user:
  *   get:
+ *     tags:
+ *     - "User"
  *     summary: example to get user
  *     responses:
  *       200:
@@ -24,12 +33,14 @@ router.delete('/:id', deleteById);
  *       500:
  *         description: Internal Server Error
  */
-router.get('/', getAll)
+router.get("/", getAll);
 
 /**
  * @swagger
  * /api/v1/user/{id}:
  *   get:
+ *     tags:
+ *     - "User"
  *     summary: Get one user
  *     parameters:
  *       - in: path
@@ -47,18 +58,35 @@ router.get('/', getAll)
  *         description: Internal Server Error
  */
 
-router.get('/:id', getById)
+router.get("/:id", getById);
 
-// /**
-//  * @swagger
-//  * /
-//  *   post:
-//  *     summary: insert user
-//  *     responses:
-//  *       200:
-//  *         description: Successful response
-//  */
-// router.post('/', CheckPostReq, Insert)
+/**
+ * @swagger
+ * /api/v1/user:
+ *   post:
+ *     tags:
+ *     - "User"
+ *     summary: example to create user
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                user_id:
+ *                  type: string
+ *                bank_name:
+ *                  type: string
+ *                bank_account_number:
+ *                  type: string
+ *                balance:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
+router.post("/", CheckPostReq, create);
 
 // /**
 //  * @swagger
